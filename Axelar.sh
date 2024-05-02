300dad94b7992d5c5842ca5a12b902bdaad53c85
Kutujaya <77893381+kutu48@users.noreply.github.com>
2024-05-02 13:41:18 +0700

    Create Axelar.sh

---
+++ Axelar.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+RPC_URL=""
+
+OUTPUT_DIR="logs"
+
+# Proxy configuration
+PROXY_HOST=""
+PROXY_PORT=""
+PROXY_USER=""
+PROXY_PASS=""
+
+axelar_data=$(curl -s --proxy "socks5://$PROXY_HOST:$PROXY_PORT" -U "$PROXY_USER:$PROXY_PASS" -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"status"}' "$RPC_URL")
+
+if [ $? -eq 0 ]; then
+    if [ ! -d "$OUTPUT_DIR" ]; then
+        mkdir "$OUTPUT_DIR"
+    fi
+    
+    echo "$axelar_data" >> "$OUTPUT_DIR/axelar.txt"
+    
+    echo "Axelar data fetched and saved to $OUTPUT_DIR/axelar.txt."
+else
+    echo "Failed to fetch Axelar data."
+fi
